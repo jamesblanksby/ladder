750a3c0ed952b0078017e19604eaead564ac0b94
James Blanksby <james@thisismyengine.com>
2018-01-02 13:23:55 +0000

    Fixed incorrect paths

--- gulpfile.js
+++ gulpfile.js
@@ -16,8 +16,8 @@ var map    = require('gulp-sourcemaps');
 
 /* glob */
 var glob = {
-    scss: '/src/scss/**/*.scss',
-    script: '/src/script/**/*.js'
+    scss: 'src/scss/**/*.scss',
+    script: 'src/script/**/*.js'
 };
 
 /* error */
